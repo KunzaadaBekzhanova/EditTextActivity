52ccfcb730d0bd938dce4204fc61ed8861d32823
SunflowerKun <dream26322gmail.com>
2022-01-06 13:51:33 +0600

    Initial commit

---
+++ app/src/main/java/kg/tutorialapp/edittextactivity/MainActivity.kt
@@ -0,0 +1,33 @@
+package kg.tutorialapp.edittextactivity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+const val EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE"
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val button1 = findViewById<Button>(R.id.showInput)
+        button1.setOnClickListener{
+            sendMessage()
+        }
+    }
+    fun sendMessage() {
+        val editText = findViewById<EditText>(R.id.editText)
+        val message = editText.text.toString()
+        val intent = Intent(this, DisplayMessageActivity::class.java).apply {
+            putExtra(EXTRA_MESSAGE, message)
+        }
+        startActivity(intent)
+        }
+
+        }
+
+
